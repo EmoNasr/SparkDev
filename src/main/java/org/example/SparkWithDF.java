31baf775af6a7f09226257794eb030acc6108acd
EmoNasr <jabarinasredinne@gmail.com>
2023-04-05 00:38:04 +0000

    TP4-p2

--- src/main/java/org/example/SparkWithDF.java
+++ src/main/java/org/example/SparkWithDF.java
@@ -2,10 +2,15 @@ package org.example;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.apache.spark.sql.functions.*;
 
 public class SparkWithDF {
 
@@ -20,32 +25,40 @@ public class SparkWithDF {
         options.put("user", "root");
         options.put("password", "");
 
-        //        DFM = ss.read().format("jdbc").options(options).option("dbtable", "medecin").load();
-        //        DFC = ss.read().format("jdbc").options(options).option("dbtable", "consultation").load();
-        //        Dataset<Row> DFJ = DFM.join(DFC, DFC.col("id_medecin").equalTo(DFM.col("id")), "inner");
-        //        DFJ.groupBy("nom", "prenom").count().show();
-                ss.read()
-                .format("jdbc")
-                .options(options).option("query", "SELECT m.nom, m.prenom, COUNT(*) AS count FROM medecin m INNER JOIN consultation c ON m.id = c.id_medecin GROUP BY m.nom, m.prenom").load().show();
 
-        //        DFC = ss.read()
-        //              .format("jdbc")
-        //              .options(options)
-        //              .option("dbtable", "consultation")
-        //              .load();
-        //        DFM = ss.read()
-        //              .format("jdbc")
-        //              .options(options)
-        //              .option("dbtable", "medecin")
-        //              .load();
-        //        Dataset<Row> DFJ1 = DFM.join(DFC, DFC.col("id_medecin")
-        //                              .equalTo(DFM.col("id")), "inner");
-        //        DFJ1.groupBy("nom", "prenom")
-        //                  .agg(countDistinct("id_patient")
-        //                  .as("nombre_patients"))
-        //                  .show();
-                ss.read()
+        Dataset<Row> datasetC = ss.read()
                 .format("jdbc")
-                .options(options).option("query", "SELECT m.nom, m.prenom, COUNT(DISTINCT c.id_patient) AS nombre_patients FROM consultation c INNER JOIN medecin m ON c.id_medecin = m.id GROUP BY m.nom, m.prenom").load().show();
+                .options(options)
+                .option("dbtable", "consultation")
+                .load();
+
+        Dataset<Row> datasetM = ss.read()
+                .format("jdbc")
+                .options(options)
+                .option("dbtable", "medecin")
+                .load();
+
+        Dataset<Row> datasetP = ss.read()
+                .format("jdbc")
+                .options(options)
+                .option("dbtable", "patients")
+                .load();
+
+        datasetC.groupBy(datasetC.col("DATA_CONSULTATION"))
+                .agg(countDistinct("id_patient").alias("ConsultationParJour"))
+                .show();
+
+        datasetM.join(datasetC,datasetC.col("id_medecin")
+                        .equalTo(datasetM.col("id")),"inner")
+                         .groupBy("nom","prenom")
+                         .agg(countDistinct("id_patient").as("NombreDePatient"))
+                     .show();
+
+        datasetM.join(datasetC,datasetC.col("id_medecin")
+                        .equalTo(datasetM.col("id")),"inner")
+                .groupBy("nom","prenom")
+                .count()
+                .show();
+
     }
 }
